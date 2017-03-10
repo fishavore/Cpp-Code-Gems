1901a13c10c7b0d2b49e219cd1425859604a08cc
ALEGRA <alexander.grahn@knowit.se>
2017-03-10 08:49:14 +0100

    Comments

--- Stuff/main.cpp
+++ Stuff/main.cpp
@@ -1,3 +1,20 @@
+/*
+////////////////////////////////////////////////////////////////////////////////////
+
+This solution was created in order to keep notes about optimisations, tips,
+tricks and other various things that could be useful one day. Micro-optimisations
+usually isn't necessary in most ways but to be a better programmer, it certainly
+wouldn't hurt to know them. I like to believe that it is better to know all the
+unnecessary stuff in order to know when *not* to use them. Good code isn't even
+the coolest syntax on the block but the one that is exactly what you were looking
+for.
+
+////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+
+
 #include <iostream>
 #include <string>
 using namespace std;
