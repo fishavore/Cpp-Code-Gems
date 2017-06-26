491f3cfeed0d9604dd38412c235fb09d4f05d265
ALEGRA <alexander.grahn@knowit.se>
2017-06-26 13:02:50 +0200

    More rvalue stuff

--- Stuff/rvaluereference.h
+++ Stuff/rvaluereference.h
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <vector>
 
+//more info at http://www.cprogramming.com/c++11/rvalue-references-and-move-semantics-in-c++11.html
+
 namespace RValueReference
 {
-
 	inline void process_copy(std::vector<int>& vec_) //bad
 	{
 		//std::vector<int> vec(vec_);
@@ -15,6 +16,14 @@ namespace RValueReference
 	inline void process_copy(std::vector<int> && vec) //good
 	{
 		vec.push_back(456);
+		
+		//The reason of why this is good is that we reduce the amount of copying.
+		//The memory is handled more effectively. Read link for further details.
+	}
+
+	void foo(int&& a)
+	{
+		//Some magical code...
 	}
 
 	inline void start()
@@ -27,5 +36,14 @@ namespace RValueReference
 		process_copy(fillThisVar); //clumsy
 		process_copy(std::move(fillThisVar2)); //ok
 		std::cout << "rvalue: " << fillThisVar[0] << " " << fillThisVar2[0] << '\n';
+
+		//
+		int b = 5;
+		//foo(b); //Error. An rValue reference cannot be pointed to a iValue.
+		foo(5); //Compiles with no error.
+		foo(b + 3); //Compiles with no error.
+
+	   // int&& c = b; //Error. An rValue reference cannot be pointed to a iValue.
+		int&& d = 5; //Compiles with no error.
 	}
 }
