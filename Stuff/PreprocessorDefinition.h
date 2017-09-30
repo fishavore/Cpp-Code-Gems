33abf565f9a20d20b80e3058c83c4759e50ae6f0
ze413X <alexander_grahn@hotmail.com>
2017-09-30 13:35:32 +0200

    Proj name in code

--- Stuff/PreprocessorDefinition.h
+++ Stuff/PreprocessorDefinition.h
@@ -4,17 +4,25 @@
 #include <string>
 namespace PreprocessorDefinition
 {
+#ifndef PROJECT_NAME
+#define PROJECT_NAME
+#endif
+
 #ifdef _ONEWAYTODOIT
 	inline void start()
 	{
-		std::cout << "Function written WITH preprocessor definition.\n";
 		// Properties -> c/c++ -> Preprocessor -> Preprocessor Definitions.
+		std::cout << "Function written WITH preprocessor definition.\n";
+		
+		//Writing the actual name of the project in visual studios inside the code. Check Preprocessor Definitions
+		//for "All configurations". It says: PROJECT_NAME="$(ProjectName)";
+		std::cout << PROJECT_NAME << '\n';
 	}
 #else
 	inline void start()
 	{
-		std::cout << "This function is written WITHOUT the preprocessor definition project settings.\n";
 		// Properties -> c/c++ -> Preprocessor -> Preprocessor Definitions.
+		std::cout << "This function is written WITHOUT the preprocessor definition project settings.\n";
 	}
 #endif
 }
