7c07fc20090a57aa9ea3634d19727d8acc2b4beb
ALEGRA <alexander.grahn@knowit.se>
2017-10-23 16:57:10 +0200

    Removed algorithm and adjusted comments.

--- Stuff/Functor.h
+++ Stuff/Functor.h
@@ -1,15 +1,14 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 /*
-Functors are essentially function objects,
-i.e. objects with the function call operator overloaded.
-Primary use of functors is in place of function pointers,
-where the functor would be able to store data 
-across multiple function calls without the use of  
-global variables.
+	Functors are essentially function objects,
+	i.e. objects with the function call operator overloaded.
+	Primary use of functors is in place of function pointers,
+	where the functor would be able to store data 
+	across multiple function calls without the use of  
+	global variables.
 */
 
 namespace Functor 
@@ -33,18 +32,16 @@ namespace Functor
 	void start()
 	{
 		std::cout << "Inside Functor.\n";
-		int size = 5;
-		int arr[] = {1, 3, 5, 7, 9};
+		const int arr[] = {1, 3, 5, 7, 9};
 		
 		// method 1 - using function and global variable
-		globalVariable = 0; 
-		// need to reset global variable everytime functionality is used
-		std::for_each(arr, arr+size, sumFunction);
+		globalVariable = 0; // need to reset global variable everytime functionality is used
+		for (int i : arr) { sumFunction(i); }
 		std::cout << "Sum is " << globalVariable << std::endl;
 		
 		// method 2 - using functor
 		SumFunctor sum;
-		std::for_each(arr, arr+size, sum);
+		for (int i : arr) { sum(i); }
 		std::cout << "Sum is " << sum.sum << std::endl;
 	}
 	
