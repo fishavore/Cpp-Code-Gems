36f1e44bec7854c2d26d244f782c5ad7e7861c52
Alexander Grahn <alexander-grahn@outlook.com>
2018-06-06 13:36:09 +0200

    DLL

--- Stuff/DLLExample/includes/DLLExampleCode.h
+++ Stuff/DLLExample/includes/DLLExampleCode.h
@@ -1,15 +1,9 @@
 #pragma once
 
-#ifdef __cplusplus  
-#define FORC extern "C" // only need to export C interface if used by C++ source code
-#else
-#define FORC 
-#endif
-
 #define DLLExport _declspec(dllexport)
 
-FORC DLLExport int __cdecl GetValue();
+extern "C" DLLExport int __cdecl GetValue();
 
-FORC DLLExport void __cdecl GetText(char* str, int strLength);
+extern "C" DLLExport void __cdecl GetText(char* str, int strLength);
 
-FORC DLLExport void __cdecl DLLPrint();
+extern "C" DLLExport void __cdecl DLLPrint();
