93954efccd137db23ce1726103f4cb858ad7f3f6
ALEGRA <alexander.grahn@knowit.se>
2019-03-19 08:57:25 +0100

    Map

--- Stuff/Map.h
+++ Stuff/Map.h
@@ -1,36 +1,60 @@
 #pragma once
 
 #include <map>
-#include <iostream>
 
-//TODO https://www.fluentcpp.com/2018/12/11/overview-of-std-map-insertion-emplacement-methods-in-cpp17/
+#include <iostream>
+#include <string>
 
 namespace Map
 {
+	void map_copy(std::map<int, std::string>&& map_)
+	{
+		std::map<int, std::string> thirdMap;
+		thirdMap[1] = "Rudolf";
+		thirdMap[2] = "Red";
+		map_.clear();
+		map_.insert(thirdMap.begin(), thirdMap.end());
+	}
+
 	void start()
 	{
-		std::map<int, unsigned int> myMap;
-
-		myMap[1] = 1;
-		myMap[2] = 2;
-		myMap[3] = 3;
-		myMap[4] = 4;
-		myMap[5] = 5;
-		myMap[6] = 6;
-		
-		for (int i = 1; i < 6; i++)
 		{
-			std::cout << myMap.at(i) << ' ';
+			std::map<int, unsigned int> myMap;
+
+			myMap[1] = 1;
+			myMap[2] = 2;
+			myMap[3] = 3;
+			myMap[4] = 4;
+			myMap[5] = 5;
+			myMap[6] = 6;
+
+			for (int i = 1; i < 6; i++)
+			{
+				std::cout << myMap.at(i) << ' ';
+			}
+
+			myMap[2] = 99;
+
+			for (int i = 1; i < 6; i++)
+			{
+				std::cout << myMap.at(i) << ' ';
+			}
+
+			std::cout << '\n';
 		}
-
-		myMap[2] = 99;
-
-		for (int i = 1; i < 6; i++)
+		//
+		//map
+		std::map<int, std::string> secMap;
+		secMap[1] = "Test1";
+		secMap[2] = "Test2";
+		for (int i = 1; i < 3; i++)
 		{
-			std::cout << myMap.at(i) << ' ';
+			std::cout << secMap.at(i) << ' '; //Test1 Test2
 		}
-
-		std::cout << '\n';
-
+		map_copy(std::move(secMap));
+		for (int i = 1; i < secMap.size() + 1; i++)
+		{
+			std::cout << secMap.at(i) << ' '; //Rudolf Red
+		}
 	}
 }
