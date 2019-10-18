b8a313fcc69aa7f0a2e4e7100f2b9b2fd37fe6c5
fishavore <fishavore@users.noreply.github.com>
2019-10-18 02:13:12 -0700

    Added the class keyword before Rectangle to avoid conflict with the Rectangle function in Wingdi.h and uncommented all lines in the start function.

--- Stuff/friends.h
+++ Stuff/friends.h
@@ -9,11 +9,11 @@ namespace Friends
 {
 	inline void start()
 	{
-		//Rectangle rect;
-		//rect.Print();
-		//Square square = Square(5, 99);
-		//std::cout << "Friends: " << square.ReturnX() << endl;
-		//rect.SetSide(square);
-		//rect.Print();
+		class Rectangle rect;
+		rect.Print();
+		Square square = Square(5, 99);
+		std::cout << "Friends: " << square.ReturnX() << endl;
+		rect.SetSide(square);
+		rect.Print();
 	}
 }
